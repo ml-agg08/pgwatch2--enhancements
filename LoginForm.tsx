398184b6f392d7830adbf35cecd53bcb58423a05
KPAVITRA-27 <127971418+KPAVITRA-27@users.noreply.github.com>
2025-03-31 08:34:16 +0530

    Update LoginForm.tsx

--- LoginForm.tsx
+++ LoginForm.tsx
@@ -1,46 +1,57 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, FormControl, FormHelperText, InputLabel, OutlinedInput, Typography, CircularProgress } from "@mui/material";
-import { PasswordInput } from "components/PasswordInput/PasswordInput";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { FormControl, InputLabel, OutlinedInput, FormHelperText, Button, Snackbar, Alert, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useLoginFormStyles } from "./LoginForm.styles";
 import { useFormStyles } from "styles/form";
+import { PasswordInput } from "components/PasswordInput/PasswordInput";
+import { useState } from "react";
 import { useLogin } from "queries/Auth";
 import { loginFormValuesValidationSchema } from "./LoginForm.consts";
-import { useLoginFormStyles } from "./LoginForm.styles";
 import { LoginFormValues } from "./LoginForm.types";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 export const LoginForm = () => {
-  const { classes: loginFormClasses, cx } = useLoginFormStyles();
-  const { classes: formClasses } = useFormStyles();
-
-  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<LoginFormValues>({
+  const navigate = useNavigate();
+  const [showSuccess, setShowSuccess] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormValues>({
     resolver: yupResolver(loginFormValuesValidationSchema),
   });
-
-  const navigate = useNavigate();
+  const { classes: loginFormClasses } = useLoginFormStyles();
+  const { classes: formClasses } = useFormStyles();
   const login = useLogin(navigate);
 
-  const getError = (field: keyof LoginFormValues) => {
-    const error = errors[field];
-    return error && error.message;
-  };
+  const getError = (field: keyof LoginFormValues) => errors[field]?.message;
 
   const onSubmit: SubmitHandler<LoginFormValues> = (values) => {
-    login.mutate(values);
+    setShowSuccess(true);
+    setTimeout(() => {
+      login.mutate(values);
+    }, 1500);
+  };
+
+  const handleCloseSuccess = () => {
+    setShowSuccess(false);
   };
 
   return (
-    <div className={loginFormClasses.root}>
+    <>
+      <Typography 
+        variant="h4" 
+        component="h1" 
+        gutterBottom 
+        align="center"
+        sx={{ 
+          fontWeight: 'bold',
+          mb: 4,
+          color: 'primary.main'
+        }}
+      >
+        Login
+      </Typography>
+      
       <form onSubmit={handleSubmit(onSubmit)} className={loginFormClasses.form}>
-        <Typography variant="h3" className={loginFormClasses.title}>
-          Welcome Back
-        </Typography>
-        <Typography variant="body1" sx={{ textAlign: "center", color: "#666", mb: 3 }}>
-          Please sign in to continue
-        </Typography>
-        
         <FormControl
-          className={cx(formClasses.formControlInput, formClasses.widthDefault, loginFormClasses.input)}
+          className={`${formClasses.formControlInput} ${formClasses.widthFull} ${loginFormClasses.input}`}
           error={!!getError("user")}
           variant="outlined"
         >
@@ -50,14 +61,11 @@ export const LoginForm = () => {
             id="user"
             label="Username"
             placeholder="Enter your username"
-            autoComplete="username"
-            autoFocus
           />
-          <FormHelperText className={loginFormClasses.error}>{getError("user")}</FormHelperText>
+          <FormHelperText>{getError("user")}</FormHelperText>
         </FormControl>
-
         <FormControl
-          className={cx(formClasses.formControlInput, formClasses.widthDefault, loginFormClasses.input)}
+          className={`${formClasses.formControlInput} ${formClasses.widthFull} ${loginFormClasses.input}`}
           error={!!getError("password")}
           variant="outlined"
         >
@@ -67,43 +75,34 @@ export const LoginForm = () => {
             id="password"
             label="Password"
             placeholder="Enter your password"
-            autoComplete="current-password"
           />
-          <FormHelperText className={loginFormClasses.error}>{getError("password")}</FormHelperText>
+          <FormHelperText>{getError("password")}</FormHelperText>
         </FormControl>
-
         <Button
           type="submit"
           variant="contained"
+          color="primary"
           className={loginFormClasses.button}
-          disabled={isSubmitting}
           fullWidth
         >
-          {isSubmitting ? (
-            <CircularProgress size={24} color="inherit" />
-          ) : (
-            "Sign In"
-          )}
+          Sign In
         </Button>
-
-        <Typography 
-          variant="body2" 
-          sx={{ 
-            textAlign: "center", 
-            color: "#666", 
-            mt: 2,
-            "& a": {
-              color: "#1a237e",
-              textDecoration: "none",
-              "&:hover": {
-                textDecoration: "underline",
-              },
-            },
-          }}
-        >
-          Forgot your password? <a href="#">Reset it here</a>
-        </Typography>
       </form>
-    </div>
+
+      <Snackbar 
+        open={showSuccess} 
+        autoHideDuration={1500} 
+        onClose={handleCloseSuccess}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert 
+          onClose={handleCloseSuccess} 
+          severity="success" 
+          sx={{ width: '100%' }}
+        >
+          Login Successful! Redirecting to Sources...
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
